f65281b2326b9434e728a1e2a599b145d8ba1608
Nguyễn [H.N] Duy <rn26-duynhn>
2022-10-23 14:49:56 +0700

    homework

---
+++ examples/examples 6/form/form.js
@@ -0,0 +1,57 @@
+const input = document.querySelector(".input");
+const eyeOpen = document.querySelector(".eye-open");
+const eyeColse = document.querySelector(".eye-close");
+eyeOpen.addEventListener("click", function () {
+  eyeOpen.classList.add("hidden")
+  eyeColse.classList.remove("hidden")
+  input.setAttribute("type", "password")
+});
+eyeColse.addEventListener("click", function () {
+    eyeOpen.classList.remove("hidden")
+    eyeColse.classList.add("hidden")
+    input.setAttribute("type", "text")
+});
+
+// function handleReset() {
+//     let elementInputEmail = document.getElementById('input__email');
+//     let elementInputPassword = document.getElementById('input__password');
+//     let elementInputCheckBox = document.getElementById('input__checkbox');
+//     if (elementInputEmail) {
+//         elementInputEmail.value = '';
+//     }
+//     if (elementInputCheckBox) {
+//         elementInputCheckBox.checked = false;
+//     }
+// }
+
+function handleSubmit(event) {
+    alert("Submit success")
+    let elementInputEmail = document.
+    getElementById('input__email');
+    // let elementInputPassword = document.getElementById('input__password');
+    // let elementInputCheckBox = document.getElementById('input__checkbox');
+    let objectUser = {
+        name: elementInputEmail?.value,
+        // createdAt: elementInputPassword?.value,
+        avatar:
+            "https://scontent.fhan14-1.fna.fbcdn.net/v/t39.30808-6/277108448_132485565988585_62266165370526473_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=d9EVZTmPEnAAX-zZK5T&_nc_ht=scontent.fhan14-1.fna&oh=00_AT-knHFcxMx1FJViTto8baZM7KIpnnWgH9LDCPxOnnkasg&oe=63584DD1",
+    }
+     console.log(objectUser);
+    postData(objectUser)
+    return false;
+}
+function postData(user) {
+    let url = "https://634e9e514af5fdff3a62367c.mockapi.io/users"
+    fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+    })
+        .then((response) => response.json())
+        .then((res) => console.log(res))
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+}
---
+++ examples/examples 6/list/list.js
@@ -0,0 +1,75 @@
+getListUsers();
+// addRowJs();
+
+function getListUsers() {
+  let url = "https://634e9e514af5fdff3a62367c.mockapi.io/users";
+  fetch(url, {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      data?.map((user) => addRowJs(user));
+      // console.log(data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+function formatRow(user) {
+  return `
+     <th>${user?.id}</th>
+     <td>${user?.name}</td>
+     <td>${user?.createdAt}</td>
+     <td>${user?.age}</td>
+     <td><button class="dt" onclick = "gotoDetail(${user?.id})">Detail</button></td>
+     <td><button class="dl" onclick = "deleteRowAPI(this,${user?.id})">Delete</button></td>
+`;
+}
+
+function addRowJs(user) {
+  // console.log(user);
+  var tableRef = document
+    .getElementById("table_users")
+    .getElementsByTagName("tbody")[0];
+  var newRow = tableRef.insertRow(tableRef.rows.length);
+  newRow.innerHTML = formatRow(user);
+}
+
+function gotoDetail(id) {
+  console.log(window.location.href, id);
+  window.location.href = "./detail.html?id ="+ id;
+}
+
+function deleteRowJs(elm) {
+  let elmRow = elm.parentElement.parentElement;
+  console.log(elmRow);
+  elmRow.remove();
+  // document.getElementById("table_users").deleteRow(index);
+}
+
+// function resetTbody() {
+//   var tableRef = document
+//     .getElementById("table_users")
+//     .getElementsByTagName("tbody")[0];
+//   tableRef.innerHTML = "";
+// }
+
+function deleteRowAPI(elm, id) {
+  // console.log("deleteRowAPI", id);
+  let url = "https://634e9e514af5fdff3a62367c.mockapi.io/users/";
+  fetch(url + id, {
+    method: "DELETE",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      deleteRowJs(elm);
+      // resetTbody();
+      // getListUsers();
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
